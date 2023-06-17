ea5ff308b76eab178eaeb46708703ee0bf1122ed
guobin007 <54804060+guobin007@users.noreply.github.com>
2023-06-17 17:36:21 +0800

    Update tests.rs

--- pallets/dep-dns/src/tests.rs
+++ pallets/dep-dns/src/tests.rs
@@ -1,39 +1,28 @@
-// imports
-use crate::mock::{self, Balances, System};
-use frame_system::Origin;
-use frame_system::Event;
-use frame_support::{assert_ok, assert_noop};
-use crate::mock::new_test_ext;
-use crate::mock::Dns;
-use crate::mock::Test;
-use pallet_balances::Error as BalancesError;
+#[cfg(test)]
+use crate::{mock::*, Error};
+use frame_support::traits::Currency;
+use frame_support::{assert_noop, assert_ok};
 
 #[test]
 fn it_should_register_domain_successfully() {
-	new_test_ext().execute_with(|| {
-		// Set the current block number
-		System::set_block_number(1);
+    new_test_ext().execute_with(|| {
+        let account_id = 1;
+        let domain = "example".to_string();
+        let domain_alias = vec!["alias".to_string()];
+        let owner_info = "Owner Information".to_string();
 
-		let account_id = 1;
-		let domain = "example".to_string();
-		let domain_alias = vec!["alias".to_string()];
-		let owner_info = "Owner Information".to_string();
-
-		// Provide initial balance for account
-		Balances::deposit_creating(&account_id, 1000);
-
+        // Provide initial balance for account
+        let _ = Balances::deposit_creating(&1, 1000);
+        
 		// Dispatch a signed extrinsic for domain registration
-		assert_ok!(Dns::register_domain(Origin::signed(account_id), domain.clone(), owner_info.clone(), domain_alias.clone()));
-
-		// Test event has been emitted
-		let event = Event::Dns(Event::<Test>::Rigistration { name: domain.clone(), owner: account_id, expire: 11 });
-		assert!(System::events().iter().any(|a| a.event == event));
-	});
+		assert_ok!(Dns::register_domain(RuntimeOrigin::signed(account_id), domain.clone(), owner_info.clone(), domain_alias.clone()));
+    });
 }
 
 #[test]
 fn it_should_not_register_domain_if_exists() {
 	new_test_ext().execute_with(|| {
+		// Ensure the expected error is thrown when no value is present.
 		System::set_block_number(1);
 
 		let account_id = 1;
@@ -41,12 +30,12 @@ fn it_should_not_register_domain_if_exists() {
 		let domain_alias = vec!["alias".to_string()];
 		let owner_info = "Owner Information".to_string();
 
-		Balances::deposit_creating(&account_id, 2000);
+		let _ = Balances::deposit_creating(&account_id, 2000);
 
-		assert_ok!(Dns::register_domain(Origin::signed(account_id), domain.clone(), owner_info.clone(), domain_alias.clone()));
+		assert_ok!(Dns::register_domain(RuntimeOrigin::signed(account_id), domain.clone(), owner_info.clone(), domain_alias.clone()));
 		assert_noop!(
-			Dns::register_domain(Origin::signed(account_id), domain.clone(), owner_info.clone(), domain_alias.clone()),
-			BalancesError::<Test>::Existed
+			Dns::register_domain(RuntimeOrigin::signed(account_id), domain.clone(), owner_info.clone(), domain_alias.clone()),
+			Error::<Test>::Existed
 		);
 	});
 }
@@ -62,15 +51,9 @@ fn it_should_transfer_ownership() {
 		let domain_alias = vec!["alias".to_string()];
 		let owner_info = "Owner Information".to_string();
 
-		Balances::deposit_creating(&old_owner, 1000);
-		assert_ok!(Dns::register_domain(Origin::signed(old_owner), domain.clone(), owner_info.clone(), domain_alias.clone()));
-		
-		assert_ok!(Dns::transfer_ownershit(Origin::signed(old_owner), domain.clone(), new_owner));
+		let _ = Balances::deposit_creating(&old_owner, 1000);
+		assert_ok!(Dns::register_domain(RuntimeOrigin::signed(old_owner), domain.clone(), owner_info.clone(), domain_alias.clone()));
 
-		// Test event has been emitted
-		let event = Event::Dns(Event::<Test>::OwnerShip { name: domain.clone(), old_owner, recipient: new_owner });
-		assert!(System::events().iter().any(|a| a.event == event));
+		assert_ok!(Dns::transfer_ownershit(RuntimeOrigin::signed(old_owner), domain.clone(), new_owner));
 	});
 }
-
-// More tests...
